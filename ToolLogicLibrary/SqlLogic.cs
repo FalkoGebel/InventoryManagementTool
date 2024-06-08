5e6200c07e0aaa0738da98f8e22f3237c9cc5bb3
Falko Gebel <falko-gebel@live.de>
2024-06-08 17:59:22 +0200

    - added library project and test project and test for SQL server connection

---
+++ ToolLogicLibrary/SqlLogic.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace ToolLogicLibrary
+{
+    public class SqlLogic
+    {
+        public static List<string> GetDatabasesFromServer(string server)
+        {
+            List<string> output;
+
+            using (SqlConnection cnn = GetOpenConnection(server))
+            {
+                output = cnn.Query<string>("SELECT [name] FROM sys.databases;").AsList();
+            }
+
+            return output;
+        }
+        private static SqlConnection GetOpenConnection(string server, string? database = null)
+        {
+            string connectionString = $@"Data Source={server};{(database != null ? $"Initial Catalog={database};" : "")}Integrated Security=SSPI;TrustServerCertificate=true;";
+
+            SqlConnection cnn = new(connectionString);
+            cnn.Open();
+
+            return cnn;
+        }
+    }
+}
